dfc323b4c0ed1ebd2d654d2968fab5ad570a2db4
Sam Tarakajian <sam@cycling74.com>
2020-07-23 17:17:23 -0400

    control for setting threshold hysteresis

--- src/internal/thresh.h
+++ src/internal/thresh.h
@@ -10,11 +10,13 @@ extern "C" {
 typedef struct {
     uint16_t _lastOutput;
     uint16_t _cutoff;
+	uint16_t _hysteresis;
     uint16_t _downThreshold;
     uint16_t _upThreshold;
 } t_thresh;
 
 void thresh_init(t_thresh *self, uint16_t cutoff, uint16_t hysteresis);
+void thresh_set_cutoff(t_thresh *self, uint16_t cutoff);
 void thresh_process(t_thresh *self, uint16_t *in, uint16_t *out);
 
 #ifdef __cplusplus
