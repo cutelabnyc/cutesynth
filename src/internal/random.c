37fdaecee837c608078a69ab73d59796b572a633
Sam Tarakajian <sam@cycling74.com>
2020-07-23 16:45:33 -0400

    fixing the random mock

--- src/internal/random.c
+++ src/internal/random.c
@@ -9,26 +9,25 @@ void random_init(t_random *self)
 {
     self->_hold = _random_next();
 	self->_mock = false;
-	self->_mockstep = false;
+	self->_mockstep = true;
 }
 
 void random_process(t_random *self, uint16_t *in, uint16_t *out)
 {
-    *out = self->_hold;
-
     if (*in) {
 		if (!self->_mock) {
 			self->_hold = _random_next();
 		} else {
-			self->_hold = self->_mockstep ? 1023 : 0;
 			self->_mockstep = !self->_mockstep;
+			self->_hold = self->_mockstep ? 1023 : 0;
 		}
     }
+    *out = self->_hold;
 }
 
 void random_set_mock(t_random *self, bool doMock)
 {
 	self->_mock = doMock;
 	self->_hold = 0;
-	self->_mockstep = false;
+	self->_mockstep = true;
 }
--- test/test_random/test.random.c
+++ test/test_random/test.random.c
@@ -50,14 +50,14 @@ void test_changing(void)
 void test_mock(void)
 {
     uint16_t in_data[6] = {
-		0, 1, 1, 1, 0, 0};
+		0, 1, 0, 1, 0, 0};
     uint16_t out_data[6];
 	uint16_t exp_data[6] = {
-		0, 0, 1023, 0, 0, 0
+		0, 0, 0, 1023, 1023, 1023
 	};
 
 	random_set_mock(&op, true);
-	run_equality_test(&op, (processor_t)random_process, in_data, out_data, exp_data, 6);
+	run_equality_test(&op, (processor_t)random_process, in_data, out_data, exp_data, 5);
 }
 
 int main(void)
@@ -66,6 +66,7 @@ int main(void)
 
   RUN_TEST(test_static);
   RUN_TEST(test_changing);
+  RUN_TEST(test_mock);
 
   return UNITY_END();
 }
