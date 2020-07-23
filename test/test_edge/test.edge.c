1872e9f5208619b1dc23eaf49d91be7a73df6c86
Sam Tarakajian <sam@cycling74.com>
2020-07-23 10:05:55 -0400

    Adding in the test utility

--- test/test_edge/test.edge.c
+++ test/test_edge/test.edge.c
@@ -1,6 +1,6 @@
 #include <unity.h>
- 
 #include <cuteop.h>
+#include <test_util.h>
 
 t_edge op;
 
@@ -19,27 +19,24 @@ void test_flat(void)
 
 void test_up(void)
 {
-    uint16_t in = 0, out;
-    edge_process(&op, &in, &out);
-    TEST_ASSERT_EQUAL_INT16(out, 0);
+    uint16_t in_data[4] = {
+        0, 0, 1, 1};
+    uint16_t out_data[4];
+    uint16_t exp_data[4] = {
+        0, 0, 1, 0};
 
-    in = 1;
-    edge_process(&op, &in, &out);
-    TEST_ASSERT_EQUAL_INT16(out, 1);
-
-    edge_process(&op, &in, &out);
-    TEST_ASSERT_EQUAL_INT16(out, 0);
+    run_equality_test(&op, (processor_t)edge_process, in_data, out_data, exp_data, 4);
 }
 
 void test_down(void)
 {
-    uint16_t in = 1, out;
-    edge_process(&op, &in, &out);
-    TEST_ASSERT_EQUAL_INT16(out, 1);
+    uint16_t in_data[4] = {
+        1, 1, 0, 0};
+    uint16_t out_data[4];
+    uint16_t exp_data[4] = {
+        1, 0, 0, 0};
 
-    in = 0;
-    edge_process(&op, &in, &out);
-    TEST_ASSERT_EQUAL_INT16(out, 0);
+    run_equality_test(&op, (processor_t)edge_process, in_data, out_data, exp_data, 4);
 }
  
 int main(void)
