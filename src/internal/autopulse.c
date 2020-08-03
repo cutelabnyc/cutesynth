ae4ac6cbb0dc00d00817142490e84f1e6037299a
Sam Tarakajian <sam@cycling74.com>
2020-08-03 12:39:24 -0400

    removed prints

--- src/internal/autopulse.c
+++ src/internal/autopulse.c
@@ -22,13 +22,10 @@ void autopulse_set_pulses_per_second(t_autopulse *self, double pulses_per_second
 void autopulse_process(t_autopulse *self, uint16_t msec_elapsed, uint16_t *out)
 {
 	double exponent = ((double) msec_elapsed * self->_fps / 1000.0);
-	printf("%f\n", exponent);
 
 	double probability = 1.0 - 1.0 / pow(2.0, exponent);
-	printf("%f\n", probability);
 
 	double factor = (double) random() / (double) RAND_MAX;
-	printf("%f\n", factor);
 
 	bool didflip =  factor < probability;
 	if (didflip) {
