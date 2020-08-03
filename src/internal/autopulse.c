e65e9243507fc81fd0dfedf1c6dc22fd6302d79d
Sam Tarakajian <sam@cycling74.com>
2020-08-03 14:02:13 -0400

    prints

--- src/internal/autopulse.c
+++ src/internal/autopulse.c
@@ -1,5 +1,6 @@
 #include "autopulse.h"
 #include "math.h"
+#include <stdio.h>
 
 void autopulse_init(t_autopulse *self)
 {
@@ -10,7 +11,7 @@ void autopulse_init(t_autopulse *self)
 /**
  * Set the average number of pulses per second.
  */
-void autopulse_set_pulses_per_second(t_autopulse *self, double pulses_per_second)
+void autopulse_set_pulses_per_second(t_autopulse *self, float pulses_per_second)
 {
 	// We're working in flips, and there's two flips per pulse
 	self->_fps = 2.0 * pulses_per_second;
@@ -21,11 +22,17 @@ void autopulse_set_pulses_per_second(t_autopulse *self, double pulses_per_second
  */
 void autopulse_process(t_autopulse *self, uint16_t msec_elapsed, uint16_t *out)
 {
-	double exponent = ((double) msec_elapsed * self->_fps / 1000.0);
+	float exponent = ((float) msec_elapsed * self->_fps / 1000.0);
 
-	double probability = 1.0 - 1.0 / pow(2.0, exponent);
+	printf("exponent: %f\n", exponent);
 
-	double factor = (double) random() / (double) RAND_MAX;
+	float probability = 1.0 - 1.0 / pow(2.0, exponent);
+
+	printf("probability: %f\n", probability);
+
+	float factor = (float) random() / (float) RAND_MAX;
+
+	printf("factor: %f\n", factor);
 
 	bool didflip =  factor < probability;
 	if (didflip) {
