8a9d0ea41de688d8d1430e7d238d8bb58de5c14f
maxardito <maxwellardito@gmail.com>
2021-08-25 10:57:31 -0400

    Added GPIO aliases

--- src/cutesynth.h
+++ src/cutesynth.h
@@ -12,12 +12,26 @@
 #include "op/fb-smooth.h"
 #include "op/phasor.h"
 
+#define INPUT 0x0
+#define DIGITAL 0x0
+#define OUTPUT  0x1
+#define ANALOG 0x1
+
+#define A0 14U
+#define A1 15U
+#define A2 16U
+#define A3 17U
+#define A4 18U
+#define A5 19U
+#define A6 20U
+#define A7 21U
+
+#define NUM_PIN_PARAMS 3
+
 /**
  * Function pointers to main init/process
  * functions, initializes in hardware files
  */
-
-
 #if MESSD_UP
 
 #include "messd-up/messd.h"
@@ -26,10 +40,29 @@
 #define NUM_OUTPUTS 4
 #define NUM_ARGS (NUM_INPUTS + NUM_OUTPUTS)
 
- // typedef void (*MAIN_init_f)(messd_t *);
- // typedef void (*MAIN_process_f)(messd_t *, uint16_t *);
+messd_t messd;
 
- // Define init, process, arg array size
+// TODO: how to define these as pin_t?
+// GPIO struct for hardware IO
+uint8_t GPIO_in[NUM_INPUTS][NUM_PIN_PARAMS] = {
+    {A6, INPUT, ANALOG}, // Clock In
+    {A3, INPUT, ANALOG}, // Downbeat in
+    {A4, INPUT, ANALOG}, // Subdivision in
+    {A7, INPUT, ANALOG}, // Phase in
+    {7, INPUT, DIGITAL}   // Metric Modulation
+};
+
+uint8_t GPIO_out[NUM_OUTPUTS][NUM_PIN_PARAMS] = {
+    {4, OUTPUT, DIGITAL},  // Clock out,
+    {12, OUTPUT, DIGITAL}, // Downbeat out
+    {10, OUTPUT, DIGITAL}, // Subdivision out,
+    {8, OUTPUT, DIGITAL}  // Phase out
+};
+
+// typedef void (*MAIN_init_f)(messd_t *);
+// typedef void (*MAIN_process_f)(messd_t *, uint16_t *);
+
+// Define init, process, arg array size
 // MAIN_init_f = MS_init;
 // MAIN_process_f = MS_process;
 
