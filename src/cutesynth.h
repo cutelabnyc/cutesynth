a38ffffa5ffcee3c1be13bebba8519eee742fb99
maxardito <maxwellardito@gmail.com>
2021-09-04 13:24:53 -0400

    Removed conditional modules

--- src/cutesynth.h
+++ src/cutesynth.h
@@ -11,31 +11,8 @@
 #include "op/avg.h"
 #include "op/fb-smooth.h"
 #include "op/phasor.h"
-
-/**
- * Function pointers to main init/process
- * functions, initializes in hardware files
- *
- * TODO: Should be in a separate file
- */
-#if MESSD_UP
 #include "messd-up/messd.h"
-
-messd_t messd;
-
-void (*MAIN_init_f)(messd_t *);
-void (*MAIN_process_f)(messd_t *, double *, double *);
-
-void MAIN_fpointers_init()
-{
-    // Define init, process, arg array size
-    MAIN_init_f = MS_init;
-    MAIN_process_f = MS_process;
-}
-
-#elif MISSED_OPPORTUNITIES
 #include "missed-opportunities/opportunity.h"
-#elif UEXKULL
 #include "uexkull/uexkull.h"
-#endif
+
 
