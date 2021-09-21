ee886dc7b17a1db7e938edfd2d167e5a0e4a74c8
Sam Tarakajian <sam@cycling74.com>
2021-09-21 15:15:27 -0400

    adding extern c guards to main includes

--- src/modules/cutemodules.h
+++ src/modules/cutemodules.h
@@ -1,3 +1,11 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "messd-up/messd.h"
 #include "missed-opportunities/opportunity.h"
 #include "uexkull/uexkull.h"
+
+#ifdef __cplusplus
+}
+#endif
