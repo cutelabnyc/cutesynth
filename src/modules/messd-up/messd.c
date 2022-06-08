0362b5abd4fed4f33f2f54e459bde8d029e2e06c
Sam Tarakajian <sam@cycling74.com>
2022-06-08 13:58:17 -0400

    add functions to messd to set the multiplier-divider of the internal PLL

--- src/modules/messd-up/messd.c
+++ src/modules/messd-up/messd.c
@@ -156,6 +156,16 @@ void MS_clock_wavelength_hint(messd_t *self, float hint)
 	phase_locked_loop_hint(&self->p_locked_loop, hint);
 }
 
+void MS_set_output_multiplier(messd_t *self, int multiplier)
+{
+    phase_locked_loop_set_numerator(&self->p_locked_loop, multiplier);
+}
+
+void MS_set_output_divisor(messd_t *self, int divisor)
+{
+    phase_locked_loop_set_denominator(&self->p_locked_loop, divisor);
+}
+
 void MS_process(messd_t *self, messd_ins_t *ins, messd_outs_t *outs)
 {
     float rootClockPhase = 0;
