8fafa7eec2c2eba0f755d606361610c5e258795d
Sam Tarakajian <sam@cycling74.com>
2022-06-27 10:47:23 -0400

    oops typo

--- src/modules/messd-up/messd.c
+++ src/modules/messd-up/messd.c
@@ -418,7 +418,7 @@ static inline void _MS_process_triggerLatchedChanges(messd_t *self, messd_ins_t 
         ||
         !ins->latchModulationToDownbeat
         ||
-        self->modulationForced;
+        self->modulationForced
         ||
         self->resetPending;
     if (shouldModulate && self->modulationPending)
